b397b5fef1de0e30204d20c12473a9d3408af173
LiudasJ <jonusas.liudas@gmail.com>
2020-07-27 21:51:21 +0300

    changes

--- data.js
+++ data.js
@@ -1,10 +1,9 @@
-console.log(window);
 window.onload = () => {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "fetchData.php", true);
-    xhr.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-            document.getElementById('table').innerHTML = this.responseText;
+    xhr.onreadystatechange = () => {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById('table').innerHTML += this.responseText;
             
         }
     }
